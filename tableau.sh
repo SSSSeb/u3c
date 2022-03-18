9849e7a0f6fd1215724eda1cc7c3f89bab2967ea
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2022-03-18 19:39:22 +0100

    corrige le tableau de sortie

--- tableau.sh
+++ tableau.sh
@@ -2,4 +2,4 @@
 
 # shellcheck disable=SC1091
 source reglages || exit 1
-awk -F'\t' '{ printf("%04d: #%03d %-10s %-20s %-20s %14s %14s %06s\n",FNR-1,$5,$6,$1,$2,$7,$3,$4)}' <"${FICHIER_POUR_FRANCK}" | tail -30
+tail -n +3 "${FICHIER_POUR_FRANCK}" | awk -F'\t' '{ printf("%04d: #%03d %-10s %-20s %-20s %14s %14s %06s\n",FNR-1,$5,$6,$1,$2,$7,$3,$4)}' | tail -30
