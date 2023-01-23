0fdc6cbd9a210e01013058a301573622fbeaaf3b
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2023-01-23 10:57:58 +0100

    put .txt suffix so that windows+click allows reading file easily

--- charge_env.bat
+++ charge_env.bat
@@ -3,6 +3,6 @@ set U3C_RFID_FILES=.\rfids_dossards
 set U3C_NO_TIMING=__no_timing__
 set U3C_BEEP_FILE=.\bell.wav
 set U3C_URA_DIR=.\U3C_URA_DIR
-set U3C_T0_FILE=.\t0_file
+set U3C_T0_FILE=.\t0_file.txt
 set U3C_JADAK_IP="localhost"
 set U3C_JADAK_PORT=9055
