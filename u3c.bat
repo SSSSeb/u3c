4b7632b59c4853c8e43322e7f03d2349d208929f
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2023-02-19 17:47:45 +0100

    u3c.bat

---
+++ u3c.bat
@@ -0,0 +1,10 @@
+set U3C_INSCRIPTIONS=.\docs\exemples\test.txt
+set U3C_RFID_FILES=.\rfids_dossards
+set U3C_NO_TIMING=__no_timing__
+set U3C_BEEP_FILE=.\bell.wav
+set U3C_URA_DIR=.\U3C_URA_DIR
+set U3C_T0_FILE=.\t0_file.txt
+set U3C_JADAK_IP="localhost"
+set U3C_JADAK_PORT=9055
+python.exe 0_init.py
+python.exe 1_rfids.py
