a681f56ed45934b643fb5cdcc3ba1e959e3e7241
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2023-02-23 19:20:57 +0100

    remove name for windows to accept renaming an existing file

--- 2_consolide.py
+++ 2_consolide.py
@@ -64,6 +64,7 @@ while True:
                             item = (rfid, timing)
                             list.append(item)
                             synthese_tmp.write(rfid + " " + timing + "\n")
+    os.remove(SYNTHESE)
     os.rename(SYNTHESE_TEMPO, SYNTHESE)
     nb_rfids = len(list)
     # print(list)
