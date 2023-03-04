d733c079d4d2a6986c6907fb00a018aa6d2f842b
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2023-03-04 21:49:17 +0100

    remove useless files

--- tableau.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC1091
-source reglages || exit 1
-tail -n +2 "${FICHIER_POUR_FRANCK}" | awk -F'\t' '{ printf("%04d: #%03d %-10s %-20s %-20s %14s %14s %06s\n",FNR-1,$5,$6,$1,$2,$7,$3,$4)}' | tail -30
