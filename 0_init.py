2ee0c1c21806e453ffe5a82f51f73a248bd255fa
Sébastien Cabaniols <sebastien.cabaniols@hpe.com>
2023-03-11 11:36:10 +0100

    python bof2

--- 0_init.py
+++ 0_init.py
@@ -81,10 +81,10 @@ reponse = input("appuyer sur Entree pour tester le son (ou 'N' pour ne pas le fa
 if reponse != "N":
     pause_time=0.03
     for i in range(0,5):
-        #playsound(BEEP_FILE, block: True)
+        playsound(BEEP_FILE, block=True)
         time.sleep(pause_time)
     for i in range(0,5):
-        playsound(BEEP_FILE, block: False)
+        playsound(BEEP_FILE, block=False)
         time.sleep(pause_time)
     print("test du son (10 bips en rafale) terminé")
 
